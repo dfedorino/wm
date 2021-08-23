eece2fd557a55fe72f320a41d4dc8a0b7ad0d61f
Dmitry <dfedorino@gmail.com>
2021-08-24 01:08:21 +0800

    Feat: implement washing machine class

---
+++ src/main/java/com/dfedorino/wm/model/WashingMachine.java
@@ -0,0 +1,13 @@
+package com.dfedorino.wm.model;
+
+public class WashingMachine {
+    private boolean isLocked;
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean isLocked) {
+        this.isLocked = isLocked;
+    }
+}
