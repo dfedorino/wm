baee5911fa4780a91ebd692ac752d0a9da1fc410
Dmitry <dfedorino@gmail.com>
2021-08-29 23:06:09 +0800

    Feat: implement links in programs controller

--- src/main/java/com/dfedorino/wm/programs/ProgramsApiController.java
+++ src/main/java/com/dfedorino/wm/programs/ProgramsApiController.java
@@ -2,6 +2,8 @@ package com.dfedorino.wm.programs;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @Log4j2
@@ -19,12 +25,22 @@ public class ProgramsApiController {
     private ProgramsService programsService;
 
     @GetMapping(value = "/programs/{programName}")
-    public ResponseEntity<Program> getProgram(@PathVariable("programName") String programName) {
-        return new ResponseEntity<>(programsService.findByName(programName), HttpStatus.OK);
+    public ResponseEntity<EntityModel<Program>> getProgram(@PathVariable("programName") String programName) {
+        Program program = programsService.findByName(programName);
+        EntityModel<Program> programEntity = EntityModel.of(program,
+                linkTo(methodOn(ProgramsApiController.class).getProgram(programName)).withSelfRel(),
+                linkTo(methodOn(ProgramsApiController.class).getPrograms()).withRel("programs"));
+        return new ResponseEntity<>(programEntity, HttpStatus.OK);
     }
 
     @GetMapping(value = "/programs")
-    public ResponseEntity<List<Program>> getPrograms() {
-        return new ResponseEntity<>(programsService.findAll(), HttpStatus.OK);
+    public ResponseEntity<CollectionModel<EntityModel<Program>>> getPrograms() {
+        List<EntityModel<Program>> programs = programsService.findAll().stream()
+                .map(program -> EntityModel.of(program,
+                        linkTo(methodOn(ProgramsApiController.class).getProgram(program.getName())).withSelfRel(),
+                        linkTo(methodOn(ProgramsApiController.class).getPrograms()).withRel("programs")))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(CollectionModel.of(programs,
+                linkTo(methodOn(ProgramsApiController.class).getPrograms()).withSelfRel()));
     }
 }
--- src/test/java/com/dfedorino/wm/programs/ProgramsApiControllerTest.java
+++ src/test/java/com/dfedorino/wm/programs/ProgramsApiControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -15,6 +14,7 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProgramsApiController.class)
@@ -25,28 +25,46 @@ class ProgramsApiControllerTest {
     private ProgramsService programsService;
     @Autowired
     private ObjectMapper objectMapper;
+    private final String apiPath = "http://localhost/api/programs";
 
     @Test
     void testGetProgram_whenExistingProgramName_then200OkAndProgramObject() throws Exception {
-        String existingProgramName = "daily";
-        Program mockProgram = new Program();
-        ReflectionTestUtils.setField(mockProgram, "name", existingProgramName);
-        when(programsService.findByName(existingProgramName)).thenReturn(mockProgram);
+        Program mockDaily = new Program("daily", 1000, 60, 2400);
+        when(programsService.findByName("daily")).thenReturn(mockDaily);
 
-        mockMvc.perform(get("/api/programs/" + existingProgramName))
+        mockMvc.perform(get("/api/programs/daily"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(mockProgram)));
+                .andExpect(content().json(objectMapper.writeValueAsString(mockDaily)))
+                .andExpect(jsonPath("_links.self.href").value(apiPath + "/daily"))
+                .andExpect(jsonPath("_links.programs.href").value(apiPath));
     }
 
     @Test
     void testGetPrograms_whenGet_then200OkAndProgramObjectList() throws Exception {
-        List<Program> mockPrograms = Arrays.asList(new Program(), new Program());
-        when(programsService.findAll()).thenReturn(mockPrograms);
+        Program mockDaily = new Program("daily", 1000, 60, 2400);
+        Program mockQuick = new Program("quick", 1000, 60, 1800);
+        List<Program> programList = Arrays.asList(mockDaily, mockQuick);
+        when(programsService.findAll()).thenReturn(programList);
 
         mockMvc.perform(get("/api/programs"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(mockPrograms)));
+                .andExpect(jsonPath("_embedded.programList").isArray())
+                .andExpect(jsonPath("_embedded.programList[0].id").value(mockDaily.getId()))
+                .andExpect(jsonPath("_embedded.programList[0].name").value(mockDaily.getName()))
+                .andExpect(jsonPath("_embedded.programList[0].waterVolume").value(mockDaily.getWaterVolume()))
+                .andExpect(jsonPath("_embedded.programList[0].waterTemperature").value(mockDaily.getWaterTemperature()))
+                .andExpect(jsonPath("_embedded.programList[0].washingTime").value(mockDaily.getWashingTime()))
+                .andExpect(jsonPath("_embedded.programList[0]._links.self.href").value(apiPath + "/" + mockDaily.getName()))
+                .andExpect(jsonPath("_embedded.programList[0]._links.programs.href").value(apiPath))
+                .andExpect(jsonPath("_embedded.programList[1].id").value(mockQuick.getId()))
+                .andExpect(jsonPath("_embedded.programList[1].name").value(mockQuick.getName()))
+                .andExpect(jsonPath("_embedded.programList[1].waterVolume").value(mockQuick.getWaterVolume()))
+                .andExpect(jsonPath("_embedded.programList[1].waterTemperature").value(mockQuick.getWaterTemperature()))
+                .andExpect(jsonPath("_embedded.programList[1].washingTime").value(mockQuick.getWashingTime()))
+                .andExpect(jsonPath("_embedded.programList[1]._links.self.href").value(apiPath + "/" + mockQuick.getName()))
+                .andExpect(jsonPath("_embedded.programList[1]._links.programs.href").value(apiPath))
+                .andExpect(jsonPath("_embedded.programList[2].id").doesNotExist());
     }
 }
